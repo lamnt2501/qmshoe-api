982a06eef30f2ca1f7e0f2ff98cb6b29c5f155ba
lamnt2501 <trung.lam.for.work@gmail.com>
2024-07-09 19:37:28 +0700

    Initial commit

---
+++ src/main/java/com/lamdangfixbug/qmshoe/QmshoeApplication.java
@@ -0,0 +1,13 @@
+package com.lamdangfixbug.qmshoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmshoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QmshoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lamdangfixbug/qmshoe/QmshoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamdangfixbug.qmshoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmshoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
