95d7495ca85bc2b29e2396d09144b37adf097d24
lamnt2501 <trung.lam.for.work@gmail.com>
2024-09-18 15:04:09 +0700

    chore: add unique constrait to discount

--- src/main/java/com/lamdangfixbug/qmshoe/discount/entity/Discount.java
+++ src/main/java/com/lamdangfixbug/qmshoe/discount/entity/Discount.java
@@ -19,7 +19,7 @@ public class Discount {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(nullable = false)
+    @Column(nullable = false,unique = true)
     private String name;
     @Column(nullable = false)
     private double value;
